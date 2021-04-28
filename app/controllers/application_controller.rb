e51c44b0b73a78e49c35b8a1e1cbd664864fccaa
Chandler Craig <hellochandlercraig@gmail.com>
2021-04-28 15:57:03 -0400

    Expand user login/logout functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,36 @@
+require 'rack-flash'
+
 class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
+  use Rack::Flash
 
   configure do
+    enable :sessions
+    set :session_secret, "gear_patrol-1234"
+
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
   get "/" do
     erb :welcome
+  end
+
+  helpers do
+    def logged_in?
+      !!current_user
+    end
+
+    def current_user
+      @user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def authentication_required
+      if !logged_in?
+        flash[:notice] = "You must be logged in."
+        redirect '/'
+      end
+    end
   end
 
 end
